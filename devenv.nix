ac34abca954ddd9e73b3d47e86db0b716b3699ec
a <a@as-Virtual-Machine.local>
2024-10-31 14:36:33 -0700

    Add gitleaks package to development environment

--- devenv.nix
+++ devenv.nix
@@ -5,7 +5,10 @@
   env.GREET = "devenv";
 
   # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
+  packages = [
+    pkgs.git
+    pkgs.gitleaks
+  ];
 
   # https://devenv.sh/languages/
   # languages.rust.enable = true;
@@ -40,6 +43,20 @@
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
+  pre-commit.hooks = {
+    gitleaks = {
+      enable = true;
+      # https://github.com/gitleaks/gitleaks/blob/8e94f98e922983d23e43c17dd9a76fd7e1095b80/.pre-commit-hooks.yaml#L4
+      entry = "gitleaks protect --verbose --redact --staged";
+    };
+    nixpkgs-fmt.enable = true;
+    trailing-whitespace = {
+      enable = true;
+      # https://github.com/pre-commit/pre-commit-hooks/blob/ed714747d7acbc5790b171702bb012af3b0fe145/.pre-commit-hooks.yaml#L203-L205
+      entry = "${pkgs.python3Packages.pre-commit-hooks}/bin/trailing-whitespace-fixer";
+      types = [ "text" ];
+    };
+  };
 
   # See full reference at https://devenv.sh/reference/options/
 }
